fa1e7dcb8ac58192d0235cafd89b371ded27f345
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2024-04-26 20:27:18 +0530

    header done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,54 +23,54 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
-          </h1>
-        </Link>
-        <form
-          onSubmit={handleSubmit}
-          className='bg-slate-100 p-3 rounded-lg flex items-center'
-        >
-          <input
-            type='text'
-            placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-           <button>
-            <FaSearch className='text-slate-600' />
-          </button>
-        </form>
-        <ul className='flex gap-4'>
-          <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
-              Home
-            </li>
-          </Link>
-          <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
-              About
-            </li>
-          </Link>
-          <Link to='/profile'>
-            {currentUser ? (
-              <img
-              className='rounded-full h-7 w-7 object-cover'
-              src={currentUser.avatar}
-              alt='profile'
-            />
-            ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
-            )}
-          </Link>
+    <header className='shadow-md' style={{background: 'linear-gradient(to right, #1E2029, #9A6AFF,  #E3E3E3)'}}>
+  <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+    <Link to='/'>
+      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className='text-white'>Urban</span>
+        <span className='text-white'>Nest</span>
+      </h1>
+    </Link>
+    <form
+      onSubmit={handleSubmit}
+      className='bg-white p-3 rounded-lg flex items-center'
+    >
+      <input
+        type='text'
+        placeholder='Search...'
+        className='bg-transparent focus:outline-none w-24 sm:w-64'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button>
+        <FaSearch className='text-9A6AFF' />
+      </button>
+    </form>
+    <ul className='flex gap-4'>
+      <Link to='/'>
+        <li className='hidden sm:inline text-zinc-950 font-medium hover:underline'>
+          Home
+        </li>
+      </Link>
+      <Link to='/about'>
+        <li className='hidden sm:inline text-zinc-950 font-medium hover:underline'>
+          About
+        </li>
+      </Link>
+      <Link to='/profile'>
+        {currentUser ? (
+          <img
+          className='rounded-full h-7 w-7 object-cover'
+          src={currentUser.avatar}
+          alt='profile'
+        />
+        ) : (
+          <li className='text-zinc-950 font-medium hover:underline'> Sign in</li>
+        )}
+      </Link>
+    </ul>
+  </div>
+</header>
 
-        </ul>
-      </div>
-    </header>
   );
 }
